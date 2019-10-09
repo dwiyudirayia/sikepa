a037cfe8ac2fae6ac6f68d3eb54171bf3d9787ce
Eka Yuda Rayi A <ecountergg@gmail.com>
2019-10-09 21:53:21 +0700

    create comment crud backend

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\Interfaces\NotificationRepositoryInterfaces;
+use App\Http\Requests\StoreCommentRequest;
+use App\Http\Requests\UpdateCommentRequest;
+use Illuminate\Http\Request;
+use App\Comment;
+
+class CommentController extends Controller
+{
+    private $notification;
+
+    public function __construct(NotificationRepositoryInterfaces $notification)
+    {
+        $this->notification = $notification;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $data = Comment::all();
+
+            return response()->json($this->notification->generalSuccess($data));
+        } catch (\Throwable $th) {
+            return response()->json($this->notification->generalFailed($th));
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCommentRequest $request)
+    {
+        try {
+            Comment::create($request->store());
+            $data = Comment::all();
+            return response()->json($this->notification->storeSuccess($data));
+        } catch (\Throwable $th) {
+            return response()->json($this->notification->storeFailed($th));
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $data = Comment::with('pages.category', 'categories')->findOrFail($id);
+
+            return response()->json($this->notification->showSuccess($data));
+        } catch (\Throwable $th) {
+            return response()->json($this->notification->showFailed($th));
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        try {
+            $data = Comment::findOrFail($id);
+
+            return response()->json($this->notification->showSuccess($data));
+        } catch (\Throwable $th) {
+            return response()->json($this->notification->showFailed($th));
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCommentRequest $request, $id)
+    {
+        try {
+            Comment::where('id', $id)->update($request->update());
+            $data = Comment::all();
+
+            return response()->json($this->notification->updateSuccess($data));
+        } catch (\Throwable $th) {
+
+            return response()->json($this->notification->updateSuccess($th));
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $data = Comment::findOrFail($id);
+            $data->delete();
+
+            $array = Comment::all();
+            return response()->json($this->notification->deleteSuccess($array));
+        } catch (\Throwable $th) {
+            return response()->json($this->notification->deleteFailed($th));
+        }
+    }
+}
---
+++ app/Http/Requests/UpdateCommentRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCommentRequest extends FormRequest
+{
+    public function messages()
+    {
+        return [
+            'name.required' => 'Nama Harus di Isi',
+            'email.required' => 'Email Harus di Isi',
+            'comment.required' => 'Komentar Harus di Isi',
+        ];
+    }
+    
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required',
+            'comment' => 'required',
+        ];
+    }
+
+    public function store()
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'comment' => $this->comment,
+        ];
+    }
+
+    public function authorize()
+    {
+        return false;
+    }
+}
