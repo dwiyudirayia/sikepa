4e47f725288bdeb11c3fb360ce0ac4d6f65c72fe
Dwi Yudi Rayi Anugrah <dwiyudirayia789@gmail.com>
2019-12-24 13:34:47 +0700

    Hapus File Gak Penting & Fix Seeder

--- database/seeds/PhotoLoginSeeder.php
+++ database/seeds/PhotoLoginSeeder.php
@@ -15,7 +15,7 @@ class PhotoLoginSeeder extends Seeder
         PhotoLogin::create([
             'created_by' => 1,
             'updated_by' => 1,
-            'image_path' => '1/5.jpg',
+            'image_path' => '1577014401/photo-login-2019-12-22-1577014401.png',
         ])
     }
 }
